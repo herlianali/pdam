fa805804ab31110b980f2ef7960eb1199488246f
herlianali <aliyasa58@gmail.com>
2022-11-07 15:41:54 +0700

    fixing cek survei

--- app/Models/Rekening.php
+++ app/Models/Rekening.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Rekening extends Model
 {
@@ -11,4 +12,16 @@ class Rekening extends Model
 
     public $timestamps = false;
     protected $table = "REKENING";
+
+    public static function getData($periode, $no_bundel, $zona, $jns_pelanggan)
+    {
+        return DB::table("REKENING")
+                    ->select('no_plg', 'nama', 'jalan', 'gang', 'nomor', 'notamb', 'no_pa', 'kd_tarif')
+                    ->whereRaw("THBL = '$periode'")
+                    ->whereRaw("NO_BUNDEL = '$no_bundel'")
+                    ->whereRaw("ZONA = '$zona'")
+                    ->whereRaw("JNS_PELANGGAN = '$jns_pelanggan'")
+                    ->orderByRaw('JNS_PELANGGAN, NO_BUNDEL, JALAN, GANG, GENAP, TO_NUMBER(NOMOR), NOTAMB, NO_PLG')
+                    ->first();
+    }
 }
