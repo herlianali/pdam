f7817d2822eb7e1ae00211a8c3889011ecea2509
herlianali <aliyasa58@gmail.com>
2022-11-09 14:34:22 +0700

    backend LapTarifBandel

--- app/Http/Controllers/LaporanTarifPerBendelController.php
+++ app/Http/Controllers/LaporanTarifPerBendelController.php
@@ -2,16 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DilM;
 use Illuminate\Http\Request;
 use App\Models\LaporanTarifPerBendel;
 use App\Models\Tarif;
+use Illuminate\Support\Facades\DB;
 
 class LaporanTarifPerBendelController extends Controller
 {
     public function index()
     {
-        $dataS  = Tarif::all();
+        $dataS = Tarif::getKode();
         return view('BAMutasiPelanggan.laporanTarifPerBendel.index', compact('dataS'))->with('i');
+    }
+
+    public function showLaporanBendel(Request $request)
+    {
+        // request{zona, zona_all, bundel, bundel_all, tarif, urut:[noplg alamat]}
+
+        if(!$request->zona_all){
+            $query1 = DilM::getDataTarifBundel()->where('zona', '=', $request->zona);
+            $tester1 = "zona by request";
+        }else{
+            $query1 = DilM::getDataTarifBundel()->whereRaw("zona not in ('000','001')");
+            $tester1 = "zona kecuali 000, 001";
+        }
+
+        if(!$request->bundel_all){
+            $query2 = $query1->where('no_bundel', '=', $request->bundle);
+            $tester2 = $tester1."Dengan no bundel";
+        }else{
+            $query2 = $query1;
+            $tester2 = $tester1."Tidak Dengan no bundel";
+        }
+
+        if($request->tarif <> "ALL"){
+            $query3 = $query2->where(DB::raw('trim(kd_tarif)'), '=', trim($request->tarif, ' '));
+            $tester3 = $tester2."Kode Tidak sama dengan All";
+        }else{
+            $query3 = $query2;
+            $tester3 = $tester2."Kode Sama Dengan ALL";
+        }
+
+        if ($request->urut == "noplg") {
+            $data = $query3->orderBy('no_plg')->get();
+            $hasil = $tester3."Urut Berdasarkan noplg";
+        }elseif($request->urut == "alamat") {
+            $data = $query3->orderBy('jalan')->get();
+            $hasil = $tester3."Urut Berdasarkan Jalan";
+        }
+
+        return response()->json([$data, $hasil]);
     }
 
     public function preview()
--- app/Models/laporanTarifPerBendel.php
+++ app/Models/laporanTarifPerBendel.php
@@ -12,12 +12,12 @@ class LaporanTarifPerBendel extends Model
     use HasFactory;
     protected $table = "DIL";
 
-    public function show()
+    public static function show()
     {
-        return DB::table($this->table)
-        ->select('DIL.no_plg','DIL.nama','DIL.jalan','DIL.gang','DIL.nomor','DIL.no_tambahan', 'DIL.kd_tarif', 'RETRIBUSI.rp_retribusi')
-        ->join ('DIL', 'DIL.kd_retribusi', '=','RETRIBUSI.kd_retribusi')
-        ->get();
+        return DB::table("DIL")
+                ->select('DIL.no_plg','DIL.nama','DIL.jalan','DIL.gang','DIL.nomor','DIL.no_tambahan', 'DIL.kd_tarif', 'RETRIBUSI.rp_retribusi')
+                ->join ('RETRIBUSI', 'DIL.kd_retribusi', '=','RETRIBUSI.kd_retribusi')
+                ->get();
     }
 
 
